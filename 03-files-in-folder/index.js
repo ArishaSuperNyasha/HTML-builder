59bb7d9639e5804ea86b7838a62116b852b15828
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2024-01-21 16:30:09 +0300

    feat: complete third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const name = path.join(__dirname, 'secret-folder');
+const { stdout } = process;
+fs.readdir(name, (_, files) => {
+  files.forEach((file) => {
+    const ext = path.extname(file).slice(1);
+    const fname = path.basename(file).match(/[a-z]{1,}(?=\.)/i);
+    fs.stat(path.join(name, file), (error, fileStats) => {
+      if (error) console.log(error);
+      else {
+        if (fileStats.isDirectory()) return;
+        stdout.write(`${fname} - ${ext} - ${(fileStats.size / 1024).toFixed(3)}kb\n`)
+      }
+    });
+  });
+});
